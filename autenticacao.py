bd99df355cd8a0a44029e559cc0c8b5885099123
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2024-11-22 10:27:05 -0300

    Adicionei tratamento de erro na página de login

--- autenticacao.py
+++ autenticacao.py
@@ -2,23 +2,26 @@ from flask import *
 from hashlib import sha256
 
 def autenticar(login, senha, mysql):
-    cursor = mysql.connection.cursor()
-    senha_criptografada = sha256(senha.encode()).hexdigest()
-    
-    cursor.execute("SELECT senha FROM administrador WHERE login = %s", (login,))
-    admin_senha = cursor.fetchone()
+    try:
+        cursor = mysql.connection.cursor()
+        senha_criptografada = sha256(senha.encode()).hexdigest()
+        
+        cursor.execute("SELECT senha FROM administrador WHERE login = %s", (login,))
+        admin_senha = cursor.fetchone()
 
-    if admin_senha:
-        if senha_criptografada == admin_senha[0]:
-            session['dynamic'] = 'adm'
-            return redirect(url_for('admin_dashboard'))
+        if admin_senha:
+            if senha_criptografada == admin_senha[0]:
+                session['dynamic'] = 'adm'
+                return redirect(url_for('admin_dashboard'))
 
-    cursor.execute("SELECT senha FROM usuario WHERE nome = %s", (login,))
-    user_senha = cursor.fetchone()
+        cursor.execute("SELECT senha FROM usuario WHERE nome = %s", (login,))
+        user_senha = cursor.fetchone()
 
-    if user_senha:
-        if senha_criptografada == user_senha[0]:
-            session['dynamic'] = 'user'
-            return redirect(url_for('user_dashboard'))
+        if user_senha:
+            if senha_criptografada == user_senha[0]:
+                session['dynamic'] = 'user'
+                return redirect(url_for('user_dashboard'))
 
-    return render_template('main.html', error='Invalid username/password')
+        return render_template('login.html', alert='Usuário ou senha inválido')
+    except:
+        return f"<h1>ERROR</h1>"
